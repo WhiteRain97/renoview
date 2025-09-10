304f6aa09c62b1524466ea5072dcc7e89edf434b
WhiteRain97 <106761567+WhiteRain97@users.noreply.github.com>
2025-09-10 14:39:05 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 // app/page.tsx
 import { redirect } from "next/navigation";
-export default function Home() {
-  redirect("/analyze");
-}
-// app/page.tsx
-import { redirect } from "next/navigation";
+
 export default function Home() {
   redirect("/analyze");
 }
